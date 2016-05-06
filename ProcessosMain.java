3a64209b1b2be2b693a9cb6405ab6585fc1e5a8a
R. Maia <rafaelmaia@Rafaels-MacBook-Pro.local>
2016-05-05 22:54:07 -0300

    Limpar terminal

--- ProcessosMain.java
+++ ProcessosMain.java
@@ -3,14 +3,17 @@ import java.util.Date;
 public class ProcessosMain {
 
   public static void main(String[] args){
-    new Processo("Conteudo sensacional");
-    // GerenciadorProcessos gp = new GerenciadorProcessos();
+    // new Processo("Conteudo sensacional");
+
+    GerenciadorProcessos gp = new GerenciadorProcessos();
     //
-    // gp.adicionarProcesso(new Processo("Conteudo sensacional"));
-    // gp.adicionarProcesso(new Processo("Conteudo da huea"));
-    // gp.adicionarProcesso(new Processo("Hauihdas"));
-    // gp.adicionarProcesso(new Processo("Testinho "));
-    // gp.adicionarProcesso(new Processo("Hahhah muito teste"));
+    gp.adicionarProcesso(new Processo("Conteudo sensacional"));
+    gp.adicionarProcesso(new Processo("Conteudo da huea"));
+    gp.adicionarProcesso(new Processo("Hauihdas"));
+    gp.adicionarProcesso(new Processo("Testinho "));
+    gp.adicionarProcesso(new Processo("Hahhah muito teste"));
+
+    System.out.print("\033[H\033[2J");
   }
 
 }
